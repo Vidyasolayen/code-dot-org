bca160059fbff7519b2d32d0972f15565f7f3943
Andrew Oberhardt <Andrew@Code.org>
2016-06-28 22:18:19 -0700

    Declare dashboard_root explicitly

--- cookbooks/cdo-apps/recipes/process_queues.rb
+++ cookbooks/cdo-apps/recipes/process_queues.rb
@@ -1,5 +1,8 @@
 poise_service 'process_queues' do
+  root = File.join node[:home], node.chef_environment
+  dashboard_root = File.join root, 'dashboard'
+
   command "bundle exec bin/sqs/process_queues config/queue_config.json.erb"
   user node[:user]
-  directory "#{root}/dashboard"
+  directory dashboard_root
 end
