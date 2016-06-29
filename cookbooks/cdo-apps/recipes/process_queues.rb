2fc54b78cbadcd3f0f4dcbab155d3eaec8b1e37d
Andrew Oberhardt <Andrew@Code.org>
2016-06-28 21:49:09 -0700

    Attempting to fix undefined method 'path_to_dashboard' error

--- cookbooks/cdo-apps/recipes/process_queues.rb
+++ cookbooks/cdo-apps/recipes/process_queues.rb
@@ -1,5 +1,5 @@
 poise_service 'process_queues' do
   command "bundle exec bin/sqs/process_queues config/queue_config.json.erb"
   user node[:user]
-  directory "#{path_to_dashboard}"
+  directory "#{root}/dashboard"
 end
